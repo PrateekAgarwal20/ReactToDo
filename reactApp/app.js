441797acc50687a3ad50644bb6b6654b199a3e40
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-06-30 15:30:06 -0700

    made list of todos

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives</p>, document.getElementById('root'));
+var dummyData = [
+                  {task: "Eat Bekfast"},
+                  {task: "Grow the Ganja"},
+                  {task: "Eat Sleep Poop"},
+                  {task: "Repeat"}
+               ]
+
+class Todo extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <li>
+        <input type="submit" value="X"/>
+        {' '}
+        {this.props.task}
+      </li>
+    )
+  }
+}
+
+
+class TodoList extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <ul>
+        {dummyData.map((item) => <Todo task={item.task} />)}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
